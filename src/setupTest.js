41132b91693fccb5f77e74b0ebb3cc089041b6c9
mkuziv <mykhaylokuziv@gmail.com>
2022-10-14 12:34:54 +0300

    Task_1: start

---
+++ src/setupTest.js
@@ -0,0 +1,4 @@
+// jest-dom adds custom matches for asserting on DOM nodes.
+// allows you to do thing like:
+// expect(element).toHaveTextContent(react/i)
+// learn more: https://github.com/testing-library/jest-dom
