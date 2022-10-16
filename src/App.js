09959e12da101f468968ee96160880beccf82e03
mkuziv <mykhaylokuziv@gmail.com>
2022-10-16 14:28:33 +0300

    Task_1: added title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Search from './components/Search/Search';
 import Posts from './components/Posts/Posts';
 import Counter from './components/Counter/Counter';
+import Title from './components/Title/Title';
 
 
 const App = () => {
@@ -33,7 +34,7 @@ const App = () => {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Title  name="World"/>
       <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <Posts posts={posts} />
       <Counter />
