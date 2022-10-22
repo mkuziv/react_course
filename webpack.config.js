08b96d21c06f0745cfb9e946a2555cec96b071cd
mkuziv <mykhaylokuziv@gmail.com>
2022-10-22 09:37:38 +0300

    Task_2: added react and babel

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+    chunkFilename: "[name].[chunkhash].js",
+    assetModuleFilename: "images/[hash][ext][query]"
+  },
+  module:{
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext][query]"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
