eba1bc72f1e3938999aa01966ec750ec40171f4c
mkuziv <mykhaylokuziv@gmail.com>
2022-10-30 15:25:20 +0200

    Task_3: added button on movie img

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Post } from '../../types/interfaces';
+import Button from '../Button/Button';
 
 import './PostItem.scss';
 
@@ -11,9 +12,28 @@ const PostItem = ({ post }: PostItemProp) => {
   const {
     name, year, genre,
   } = post;
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
     <article className="movie">
-      <div className="img">img</div>
+      <div
+        className="img"
+        onMouseOver={handleMouseOver}
+        onFocus={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onBlur={handleMouseOut}
+      >
+        img
+        {isHovering && <Button name="" type="button" btn="round" />}
+      </div>
       <div className="description">
         <div className="name">
           <span>{name}</span>
