2c2d11033f7eeddbdda6bb2b4324811442311afb
mkuziv <mykhaylokuziv@gmail.com>
2022-11-06 16:28:14 +0200

    Task_4: added add_movie

---
+++ src/components/AddMovie/AddMovie.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+import './AddMovie.scss';
+
+const AddMovie = () => (
+  <section className="add-movie">
+    <h3 className="h3">add movie</h3>
+    <form action="" className="form">
+      <div className="main">
+        <div className="left-side">
+          <label htmlFor="title">
+            title
+            <br />
+            <input type="text" id="title" />
+          </label>
+          <label htmlFor="url">
+            movie url
+            <br />
+            <input type="text" id="url" placeholder="https://" />
+          </label>
+          <label htmlFor="genre">
+            genre
+            <br />
+            <select name="" id="genre">
+              <option value="select genre">Select Genre</option>
+              <option value="documentary">Documentary</option>
+              <option value="comedy">Comedy</option>
+              <option value="horror">Horror</option>
+              <option value="crime">Crime</option>
+            </select>
+          </label>
+        </div>
+        <div className="right-side">
+          <label htmlFor="date">
+            release date
+            <br />
+            <input
+              type="date"
+              id="date"
+              value="Select Date"
+            />
+          </label>
+          <label htmlFor="rating">
+            rating
+            <br />
+            <input type="number" id="rating" />
+          </label>
+          <label htmlFor="runtime">
+            runtime
+            <br />
+            <input type="number" id="runtime" placeholder="minutes" />
+          </label>
+        </div>
+      </div>
+      <label htmlFor="overview">
+        overview
+        <textarea
+          id="overview"
+          rows={5}
+          cols={30}
+        >
+          Movie description
+        </textarea>
+      </label>
+    </form>
+    <div className="add-movie__button">
+      <Button content="reset" btn="btn bnt-transparent btn-middle" type="button" />
+      <Button content="submit" btn="btn btn-red btn-middle" type="submit" />
+    </div>
+  </section>
+);
+
+export default AddMovie;
