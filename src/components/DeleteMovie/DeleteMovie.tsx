77ff0c0608b974546f6df5ff4a09f566a2a774a7
mkuziv <mykhaylokuziv@gmail.com>
2022-11-06 10:54:13 +0200

    Task_4: added delete movie

---
+++ src/components/DeleteMovie/DeleteMovie.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+import './DeleteMovie.scss';
+
+const DeleteMovie = () => (
+  <section className="delete-movie">
+    <h3 className="h3">Delete Movie</h3>
+    <p>Are you sure you want to delete this movie?</p>
+    <div className="delete-movie__button">
+      <Button content="confirm" btn="btn btn-red btn-middle" type="button" />
+    </div>
+  </section>
+
+);
+
+export default DeleteMovie;
