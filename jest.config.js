fc7c784b1de09c298cb50ca1ba270f551fd4d353
mkuziv <mykhaylokuziv@gmail.com>
2023-01-10 16:30:03 +0200

    Task_9: added jest and react testing library

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '\\.[jt]sx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/styleMock.js',
+  },
+};
